464f5f0dd981225c437c28fd66f1b79ed21c44f0
Rastko <rastkodjurica@yahoo.com>
2021-01-26 15:53:00 -0500

    rastko

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,62 @@
 // Soldier
-class Soldier {}
+
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name;
+    }
 
-// Saxon
-class Saxon {}
+    receiveDamage(damage) {
+        this.health -= damage;
+        return this.health > 0 ? `${this.name} has received ${damage} points of damage` : `${this.name} has died in act of combat`
+    }
+
+    battleCry() {
+        return 'Odin Owns You All!'
+    }
+}
+
+// Saxons
+class Saxon { }
+
+// class Saxon extends Soldier {
+//     constructor(healthArg, strengthArg) {
+//       super(healthArg, strengthArg)
+//     }
+
+//     receiveDamage(damage) {
+//       this.health -= damage;
+//       return this.health > 0 ? `A Saxon has received ${damage} points of damage` : 'A Saxon has died in combat'
+//     }
+//   }
+
+
 
 // War
-class War {}
+class War { }
+
+// class Saxon extends Soldier {
+//     constructor(healthArg, strengthArg) {
+//       super(healthArg, strengthArg)
+//     }
+
+//     receiveDamage(damage) {
+//       this.health -= damage;
+//       return this.health > 0 ? `A Saxon has received ${damage} points of damage` : 'A Saxon has died in combat'
+//     }
+//   }
